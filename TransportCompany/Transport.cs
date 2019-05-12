30d1d1acd663035a52e455721a11b1f4784e809f
Nyasyuk <48404365+Nyasyuk@users.noreply.github.com>
2019-05-13 02:36:31 +0400

    Add files via upload

---
+++ TransportCompany/Transport.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompany
+{
+    
+    abstract class Transport
+    {
+        int speed;
+        int loadCapacity;
+        string transportMark;
+        int numberOfStaff;
+        int numberOfPassengers;
+
+        public abstract void Delivery();
+        public abstract void ShowInformation();
+
+        public Transport(int speed, int loadCapacity, string transportMark, int numberOfStaff, int numberOfPassengers)
+        {
+            Speed = speed;
+            LoadCapacity = loadCapacity;
+            TransportMark = transportMark;
+            NumberOfStaff = numberOfStaff;
+            NumberOfPassengers = numberOfPassengers;
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                //Исключение ввода некорректных данных
+                if(value > 0)
+                    speed = value;
+            }
+        }
+
+        public int LoadCapacity
+        {
+            get
+            {
+                return loadCapacity;
+            }
+            set
+            {
+                //Исключение ввода некорректных данных
+                if (value > 0)
+                    loadCapacity = value;
+            }
+        }
+
+        public string TransportMark
+        {
+            get
+            {
+                return transportMark;
+            }
+            set
+            {
+                    transportMark = value;
+            }
+        }
+
+        public int NumberOfStaff
+        {
+            get
+            {
+                return numberOfStaff;
+            }
+            set
+            {
+                //Исключение ввода некорректных данных
+                if (value > 0)
+                    numberOfStaff = value;
+            }
+        }
+
+        public int NumberOfPassengers
+        {
+            get
+            {
+                return numberOfPassengers;
+            }
+            set
+            {
+                //Исключение ввода некорректных данных
+                if (value > 0)
+                    numberOfPassengers = value;
+            }
+        }
+    }
+}
---
+++ TransportCompany/WaterTransport.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompany
+{
+
+    class WaterTransport : Transport
+    {
+        public WaterTransport(int speed, int loadCapacity, string transportMark, int numberOfStaff, int numberOfPassengers)
+            : base(speed, loadCapacity, transportMark, numberOfStaff, numberOfPassengers)
+        { }
+
+        public override void Delivery()
+        {
+            Console.WriteLine("\nГруз водным транспортом доставлен!\n");
+        }
+        public override void ShowInformation()
+        {
+            Console.WriteLine("\nИнформация о транспорте.\nВодный транспорт\nСкорость: " + Speed + " Км/ч\nГрузоподъёмность: " + LoadCapacity + " Кг\nМарка: " + TransportMark + "\nПерсонал: " + NumberOfStaff + "\nПассажиры: " + NumberOfPassengers+ "\n");
+        }
+    }
+
+    class Liner : WaterTransport
+    {
+        public Liner(int speed, int loadCapacity, string transportMark, int numberOfStaff, int numberOfPassengers)
+            : base(speed, loadCapacity, transportMark, numberOfStaff, numberOfPassengers)
+        { }
+    }
+
+    class Tanker : WaterTransport
+    {
+        public Tanker(int speed, int loadCapacity, string transportMark, int numberOfStaff, int numberOfPassengers)
+            : base(speed, loadCapacity, transportMark, numberOfStaff, numberOfPassengers)
+        { }
+    }
+}
